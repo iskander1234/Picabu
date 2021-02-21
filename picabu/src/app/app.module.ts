7870be87702f4964292304f9281269add5ea319c
Iskander Dosgali <idosgali@gmail.com>
2021-02-21 20:42:22 +0600

    Добавил в app.module.ts найменования компонентов точнее классы

--- picabu/src/app/app.module.ts
+++ picabu/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {AddNewsComponent} from './addnews/addnews.component';
+import {FormsModule} from '@angular/forms';
+import {ContentComponent} from './content/content.component';
+import {LoginComponent} from './ authentication/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddNewsComponent,
+    ContentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
