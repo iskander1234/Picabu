e33848afe77738cd97b3fa4fbf93cd5f1f4495e0
Iskander Dosgali <idosgali@gmail.com>
2021-03-05 23:13:49 +0600

    Добавил компонет сообщество  в роуты

--- picabu/src/app/app.route.ts
+++ picabu/src/app/app.route.ts
@@ -1,8 +1,10 @@
 import {Routes} from "@angular/router";
 import {HotterComponent} from './hotter/hotter.component';
 import {WelcomeComponent} from './welcome/welcome.component';
+import {CommunitiesComponent} from './communities/communities.component';
 
 export const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'hotter', component: HotterComponent}
+  {path: 'hotter', component: HotterComponent},
+  {path: 'communities', component: CommunitiesComponent},
 ];
