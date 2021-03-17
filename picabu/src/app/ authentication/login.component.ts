05fad737de046f142b7d9cba8f9ff5354f4b97a5
Iskander Dosgali <idosgali@gmail.com>
2021-03-18 00:16:47 +0600

    От редактировал компоненты

--- picabu/src/app/ authentication/login.component.ts
+++ picabu/src/app/ authentication/login.component.ts
@@ -1,8 +1,16 @@
-import { Component , Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {createViewChild} from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
+   name?: string;
+   @ViewChild('nameRef') nameElementRef?: ElementRef;
+
+  // tslint:disable-next-line:typedef
+   ngAfterViewInput(){
+     this.nameElementRef?.nativeElement.focus();
+   }
 }
--- picabu/src/app/communities/communities.component.ts
+++ picabu/src/app/communities/communities.component.ts
@@ -8,8 +8,8 @@ export class CommunitiesComponent {
     title : string = 'Все сообщества';
 
   communitiess : Array<any> = [
-    {fileName : 'https://cs7.pikabu.ru/images/community/2708/1564735954248754509.png', communitiesName : 'Фабрика Мемов', descriptions : 'ААААААА'},
-    {fileName : 'https://cs13.pikabu.ru/images/community/1360/1605016081289356325.png', communitiesName : 'Специфический юмор', descriptions : 'BBBBBBB'},
-    {fileName : 'https://cs14.pikabu.ru/images/community/544/1613311861254992080.png', communitiesName : 'Наука | Научпоп', descriptions : 'CCCCCCC'},
+    {fileName : 'https://cs7.pikabu.ru/images/community/2708/1564735954248754509.png', communitiesName : 'Фабрика Мемов', descriptions : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'},
+    {fileName : 'https://cs13.pikabu.ru/images/community/1360/1605016081289356325.png', communitiesName : 'Специфический юмор', descriptions : 'Contrary to popular belief, Lorem Ipsum is not simply random text.'},
+    {fileName : 'https://cs14.pikabu.ru/images/community/544/1613311861254992080.png', communitiesName : 'Наука | Научпоп', descriptions : 'There are many variations of passages of Lorem Ipsum available.'},
   ];
 }
