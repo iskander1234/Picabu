a941f2e785350fb58eb404a68edee02e4f13a8e7
Iskander Dosgali <idosgali@gmail.com>
2021-04-09 21:11:15 +0600

    Дал роутам если полтзователь не зарегестрирован не может переходить по страницам

--- picabu/src/app/app.route.ts
+++ picabu/src/app/app.route.ts
@@ -2,9 +2,16 @@ import {Routes} from "@angular/router";
 import {HotterComponent} from './hotter/hotter.component';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {CommunitiesComponent} from './communities/communities.component';
+import {AuthGuard} from './auth/auth.guard';
 
 export const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'hotter', component: HotterComponent},
-  {path: 'communities', component: CommunitiesComponent},
+  {
+    path: 'hotter',
+    component: HotterComponent,
+    canActivate:[AuthGuard]},
+  { path: 'communities',
+    component: CommunitiesComponent,
+    canActivate:[AuthGuard]
+  },
 ];
