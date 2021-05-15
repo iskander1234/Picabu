76122fc5ba2c8d43128baa71f23a9ed6b5b27801
Iskander Dosgali <idosgali@gmail.com>
2021-05-15 18:11:08 +0600

    Добавил forms для валидаций входа в приложения

--- picabu/src/app/ authentication/login.component.ts
+++ picabu/src/app/ authentication/login.component.ts
@@ -1,16 +1,51 @@
 import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {createViewChild} from '@angular/compiler/src/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-   name?: string;
-   @ViewChild('nameRef') nameElementRef?: ElementRef;
+
+  // tslint:disable-next-line:variable-name
+  _exform : any;
+
+  // tslint:disable-next-line:typedef use-lifecycle-interface
+  ngOnInit() {
+
+    this._exform = new FormGroup({
+      'email' : new FormControl(null, [Validators.required, Validators.email]),
+      'password' : new FormControl(
+        null,
+        [
+          Validators.required,
+          Validators.pattern('^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$')
+        ])
+    });
+  }
 
   // tslint:disable-next-line:typedef
-   ngAfterViewInput(){
-     this.nameElementRef?.nativeElement.focus();
-   }
+  clicksub() {
+    console.log(this._exform.value);
+    const reset = this._exform.reset();
+  }
+
+  // tslint:disable-next-line:typedef
+  get email() {
+    return this._exform.get('email');
+  }
+  // tslint:disable-next-line:typedef
+  get password() {
+    return this._exform.get('password');
+  }
+
+
+  //  name?: string;
+  //  @ViewChild('nameRef') nameElementRef?: ElementRef;
+  //
+  // // tslint:disable-next-line:typedef
+  //  ngAfterViewInput(){
+  //    this.nameElementRef?.nativeElement.focus();
+  //  }
 }
